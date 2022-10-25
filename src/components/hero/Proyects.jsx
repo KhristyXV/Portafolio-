2d2903c5e5795faba2dd167682eb0ca60e31c32d
unknown <santiagoschroh@gmail.com>
2022-10-25 19:26:32 -0300

    componente Proyects version desktop

--- src/components/hero/Proyects.jsx
+++ src/components/hero/Proyects.jsx
@@ -3,13 +3,26 @@ import styled from "styled-components";
 import blackArrow from "../../assets/images/blackArrow.png";
 import digitize from "../../assets/videos/digitize.mp4";
 import digitizeMobile from "../../assets/images/digitizeMobile.jpg";
-import city from "../../assets/images/city.jpg";
+import city from "../../assets/videos/appClima.mp4";
 
 export const Proyects = () => {
   return (
     <Wrapper>
-      <h1>Proyectos</h1>
-
+      <div className="titleAndButton">
+        <div className="moveTitleAndButton">
+          <h1>Proyectos</h1>
+          <button className="buttonDesktop">
+            <span className="center">
+              Ver más en GitHub
+              <img
+                src={blackArrow}
+                alt="arrowBlack"
+                className="arrowBlacksin"
+              />
+            </span>
+          </button>
+        </div>
+      </div>
       <div className="boxOne">
         <video
           className="resolutionImage"
@@ -22,12 +35,20 @@ export const Proyects = () => {
         </video>
       </div>
       <div className="boxTwo">
-        <img src={city} className="resolutionImage" />
+        <video
+          className="resolutionImage"
+          id="player"
+          playsinline
+          controls
+          data-poster="/path/to/poster.jpg"
+        >
+          <source src={city} type="video/mp4" />
+        </video>
       </div>
       <div className="boxThree">
         <img src={digitizeMobile} className="resolutionImage" />
       </div>
-      <button>
+      <button className="buttonMobile">
         <span className="center">
           Ver más en GitHub
           <img src={blackArrow} alt="arrowBlack" className="arrowBlacksin" />
@@ -40,7 +61,6 @@ export const Proyects = () => {
 const Wrapper = styled.div`
   display: flex;
   justify-content: space-around;
-  height: 750px;
   align-items: center;
   flex-direction: column;
   text-align: center;
@@ -70,12 +90,13 @@ const Wrapper = styled.div`
   div {
     cursor: pointer;
   }
-  .boxContainer {
+  .buttonDesktop {
+    display: none;
   }
   .boxOne {
     width: 279px;
     height: 160px;
-    border: 3px solid #00ffe0;
+    border: 1px solid #00ffe0;
     background: url();
     box-shadow: -6px -6px 4px 2px rgba(0, 255, 224, 0.6);
   }
@@ -85,14 +106,14 @@ const Wrapper = styled.div`
   .boxTwo {
     width: 279px;
     height: 160px;
-    border: 3px solid #00ffe0;
+    border: 1px solid #00ffe0;
     box-shadow: 6px 6px 4px 2px rgba(0, 255, 224, 0.6);
   }
   .boxThree {
     width: 279px;
     height: 160px;
-    border: 3px solid #00ffe0;
-    border: 3px solid #00ffe0;
+    border: 1px solid #00ffe0;
+
     box-shadow: -6px 6px 4px 2px rgba(0, 255, 224, 0.6);
   }
   .arrowBlacksin {
@@ -105,5 +126,51 @@ const Wrapper = styled.div`
   .resolutionImage {
     width: 100%;
     height: 100%;
+  }
+  @media screen and (min-width: 1024px) {
+    margin-top: 210px;
+    .buttonMobile {
+      display: none;
+    }
+    .buttonDesktop {
+      display: block;
+    }
+    .titleAndButton {
+      display: flex;
+      width: 60%;
+    }
+    .moveTitleAndButton {
+      justify-content: left;
+    }
+    button {
+      width: 220px;
+    }
+    h1 {
+      padding-right: 20px;
+      font-family: "DM Sans";
+      font-style: normal;
+      font-weight: 500;
+      font-size: 38px;
+      line-height: 32px;
+      letter-spacing: 1.25px;
+    }
+    .boxOne {
+      width: 600px;
+      height: 350px;
+      border: 1px solid #00ffe0;
+      box-shadow: -12px -12px 8px 4px rgba(0, 255, 224, 0.6);
+    }
+    .boxTwo {
+      width: 600px;
+      height: 350px;
+      border: 1px solid #00ffe0;
+      box-shadow: 12px 12px 8px 4px rgba(0, 255, 224, 0.6);
+    }
+    .boxThree {
+      width: 600px;
+      height: 350px;
+      border: 1px solid #00ffe0;
+      box-shadow: -12px 12px 8px 4px rgba(0, 255, 224, 0.6);
+    }
   }
 `;
