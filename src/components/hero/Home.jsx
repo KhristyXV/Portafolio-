a20c3323638f2c877d6580f6497d4d53240faade
unknown <santiagoschroh@gmail.com>
2022-11-12 15:07:12 -0300

    errores

--- src/components/hero/Home.jsx
+++ src/components/hero/Home.jsx
@@ -208,8 +208,8 @@ const HomeContainer = styled.div`
       display: block;
       white-space: nowrap;
       border-right: 1px solid;
-      width: 15px;
-      animation: typing 3s steps(18), blink 0.5s infinite step-end alternate;
+      width: 20ch;
+      animation: typing 2400ms steps(19), blink 0.5s infinite step-end alternate;
       overflow: hidden;
     }
     .linkedinImage {
